c0eca34ea4b67a63b951204a08449da104482a25
YYUUJJUUNN <cdsa0555@gmail.com>
2026-01-26 11:54:59 +0900

    변경x

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { registerRootComponent } from 'expo';
-import App from './App';
+import App from './App'; // App.tsx를 불러옵니다.
 
-// 웹 전용 진입점 설정
+// Expo가 Native와 Web 환경 모두에서 적절한 진입점을 찾도록 해줍니다.
 registerRootComponent(App);
