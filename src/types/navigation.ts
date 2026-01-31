af74a94d65c0324722ee3e723a707b9accf31610
YYUUJJUUNN <cdsa0555@gmail.com>
2026-02-01 00:13:46 +0900

    페이지명 변경에 따른 변수 변경 : Compare > MyEstimate, 새로운 페이지 생성 : MyChat

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -2,15 +2,16 @@ import { UploadedImage } from './common';
 
 // 각 화면으로 변동할 때의 매개변수들을 정의
 export type RootStackParamList = {
-  Main: undefined;
-  UserSelect: { 
+  Main: undefined; // 메인으로 이동할 때는 매개변수 없음
+  UserSelect: { // UserSelect로 이동할 때는 images와 estimateId가 필요함
     images: UploadedImage[];
     estimateId: number;
   };
-  Result: { 
+  Result: { // Result로 이동할 때는 data와 estimateId와 ResultOfUserSelect가 필요함
     data: UploadedImage[];
     estimateId: number | null;
     ResultOfUserSelect: any; 
   };
-  Compare: undefined;
+  MyEstimate: undefined; // MyEstimate로 이동할 때는 매개변수 없음
+  MyChat: undefined; // MyChat으로 이동할 때는 매개변수 없음
 };
