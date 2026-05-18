586ab3f8a7edbb19ef745a55136975e55d433d39
hyotatoFrappuccino <hjkiln0@gmail.com>
2026-05-18 14:39:45 +0900

    feat: 앱 환경에서 항상 프로덕션 URL 사용
    Platform.OS 기준으로 네이티브는 하드코딩된 프로덕션 도메인 사용,
    웹은 기존처럼 환경변수 기반으로 동작

--- src/utils/Server.ts
+++ src/utils/Server.ts
@@ -1,2 +1,8 @@
-// --- 백엔드 도메인 설정 ---
-export const BACKEND_DOMAIN = process.env.EXPO_PUBLIC_BACKEND_DOMAIN ?? 'http://localhost:8080';
+import { Platform } from 'react-native';
+
+const PRODUCTION_DOMAIN = 'https://api.isajjim.kro.kr';
+
+// 앱(네이티브) 환경은 항상 프로덕션 URL 사용
+export const BACKEND_DOMAIN = Platform.OS === 'web'
+  ? (process.env.EXPO_PUBLIC_BACKEND_DOMAIN ?? 'http://localhost:8080')
+  : PRODUCTION_DOMAIN;
